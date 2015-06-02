0dccfa5411086346822499030083ba3a7b85389a
Alex Rakoczy <scissorjammer@gmail.com>
2015-06-01 20:59:46 -0400

    Add documentation to AmplitudeAPI and AmplitudeAPI::Event
    https://github.com/toothrot/amplitude-api/issues/4

--- lib/amplitude-api.rb
+++ lib/amplitude-api.rb
@@ -9,13 +9,32 @@ class AmplitudeAPI
   USER_WITH_NO_ACCOUNT = "user who doesn't have an account"
 
   class << self
+    # @!attribute [ rw ] api_key
+    #   @return [ String ] an Amplitude API Key
     attr_accessor :api_key
 
+    # Send a single event immediately to the AmplitudeAPI
+    #
+    # @param [ String ] event_name a string that describes the event, e.g. "clicked on Home"
+    # @param [ String ] user a string or integer that uniquely identifies a user.
+    # @param [ Hash ] properties a hash that is serialized to JSON, and can contain any other property to be stored on the Event
+    #
+    # @return [ Typhoeus::Response ]
     def send_event(event_name, user, properties = {})
       event = AmplitudeAPI::Event.new(user_id: user, event_type: event_name, event_properties: properties)
       track(event)
     end
 
+    # @overload body(event)
+    #   @param [ AmplitudeAPI::Event ]
+    #
+    # @overload body([events])
+    #   @param [ Array<AmplitudeAPI::Event> ]
+    #
+    # @return [ Hash ]
+    #
+    # Converts a series of AmplitudeAPI::Event objects into a body
+    # suitable for the Amplitude API
     def body(*events)
       event_body = events.flatten.map do |event|
         event.to_hash
@@ -26,6 +45,15 @@ class AmplitudeAPI
       }
     end
 
+    # @overload body(event)
+    #   @param [ AmplitudeAPI::Event ] Send a single event to the Amplitude API
+    #
+    # @overload body([events])
+    #   @param [ Array<AmplitudeAPI::Event> ] Send an array of events in a single request to Amplitude
+    #
+    # @return [ Typhoeus::Response ] 
+    #
+    # Send one or more Events to the Amplitude API
     def track(*events)
       Typhoeus.post(URI_STRING, body: body(events))
     end
--- lib/amplitude-api/event.rb
+++ lib/amplitude-api/event.rb
@@ -1,7 +1,20 @@
 class AmplitudeAPI
   class Event
-    attr_accessor :user_id, :event_type, :event_properties
+    # @!attribute [ rw ] user_id
+    #   @return [ String ] the user_id to be sent to Amplitude
+    attr_accessor :user_id
+    # @!attribute [ rw ] event_type
+    #   @return [ String ] the event_type to be sent to Amplitude
+    attr_accessor :event_type
+    # @!attribute [ rw ] event_properties
+    #   @return [ String ] the event_properties to be attached to the Amplitude Event
+    attr_accessor :event_properties
 
+    # Create a new Event
+    #
+    # @param [ String ] user_id
+    # @param [ String ] event_type
+    # @param [ Hash ] event_properties
     def initialize(user_id: , event_type: , event_properties: {})
       self.user_id = user_id
       self.event_type = event_type
@@ -17,6 +30,9 @@ class AmplitudeAPI
         end
     end
 
+    # @return [ Hash ] A serialized Event
+    #
+    # Used for serialization and comparison
     def to_hash
       {
         event_type: self.event_type,
@@ -25,6 +41,9 @@ class AmplitudeAPI
       }
     end
 
+    # @return [ true, false ]
+    #
+    # Compares +to_hash+ for equality
     def ==(other)
       if other.respond_to?(:to_hash)
         self.to_hash == other.to_hash
